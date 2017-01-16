11d38cd38d67c22e45e85ad2889e42118473590c
favl <favl@CB0358>
2017-01-16 15:45:54 +0300

    Add project files.

---
+++ BastardFat.ProcessSniffer.Processes/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BastardFat.ProcessSniffer.Processes
+{
+    public class Class1
+    {
+    }
+}
